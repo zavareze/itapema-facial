36545cbae35561c869ba2591f34a6be3af643509
Zavareze <github@zavareze.com.br>
2024-03-01 19:20:33 -0300

    initial commit

---
+++ app/cadastro/page.js
@@ -0,0 +1,225 @@
+"use client"
+import { useState } from 'react'
+import InputMask from 'react-input-mask';
+async function cadastro(req) {
+  const body = JSON.stringify(req);
+  const res = await fetch(`https://facial.parquedasaguas.com.br/cadastro/titulo`, {
+    // mode: 'no-cors',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('token')
+    },
+    body,
+  });
+  console.log(res);
+  const json = await res.json();
+  console.log(json);
+  localStorage.setItem('token', json['token']);
+  Router.push('/cadastro');
+  return json
+}
+export default function Cadastro() {
+  const [showModal, setShowModal] = useState(false);
+  const [registro, setRegistro] = useState({
+    matricula: '',
+    nome: '',
+    cpf: '', 
+    data_nascimento: '',
+    celular: '',
+    email: '',
+    cidade: '',
+    facial: '',
+    updated: ''
+  });
+  const [updated, setUpdated] = useState({});
+  const handleOnChange = event => {
+    const { name, value } = event.target;
+    setUpdated({ ...updated, [name]: value });
+  };
+  const parseJWT = (token) => {
+      var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+      return JSON.parse(jsonPayload);
+  }
+  const jwt = parseJWT(localStorage.getItem('token'));
+  const [titulos, setTitulos] = useState(jwt['titulo']);
+  const mostraModal = (person) => {
+    setRegistro(person);
+    setUpdated(person);
+    setShowModal(true);
+  }
+  const salvar = () => {
+    console.log(updated);
+    cadastro(updated);
+    setShowModal(false);
+  }
+  return (
+    <>
+      <div>
+        <div className="pb-4">
+        <p className="text-sm">
+          Você deve finalizar o cadastro e fazer o reconhecimento 
+          facial de todas que estão na cor rosa para que as 
+          mesmas possam acessar o clube.
+        </p>
+        <p className="text-sm">
+          Vale lembrar que as mesmas devem também estar em
+          dia com as taxas para poder acessar o clube</p>
+        </div>
+        <h1 className="text-lg font-bold">Relação de Pessoas no Título</h1>
+        <ul role="list" className="divide-y sm:grid sm:grid-cols-3 sm:gap-4 cursor-pointer">
+          {titulos.map((person) => (
+            <li key={person.email} className={'flex justify-between gap-x-6 py-2 '+ (person.facial=="1"?'bg-green-200':'bg-red-200')}
+              onClick={() => {mostraModal(person)}} key={person.matricula}>
+              <div className="flex min-w-0 gap-x-4 px-2">
+                <div className="w-24 flex-none">
+                  <img className="w-40 h-32 rounded-lg" src={'https://sistema.parquedasaguas.com.br/fotos.php?matricula='+person.matricula} alt="" />
+                </div>
+                <div className="min-w-0 flex-auto">
+                  <p className="text-sm font-semibold leading-6 text-gray-900">{person.nome}</p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.cpf}</p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.celular}</p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">Atualizado: {person.updated}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    {registro.nome}
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-4 flex-auto">
+                  <div className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2">
+                  <div>
+                      <label htmlFor="cpf" className="block text-sm font-semibold leading-4 text-gray-900">
+                        CPF
+                      </label>
+                      <div className="mt-2">
+                        <InputMask
+                          mask="999.999.999-99"
+                          type="text"
+                          name="cpf"
+                          id="cpf"
+                          defaultValue={registro.cpf}
+                          onChange={handleOnChange}
+                          onChange={(event) => console.log(event.target.value)}
+                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label htmlFor="data_nascimento" className="block text-sm font-semibold leading-4 text-gray-900">
+                        Data Nascimento
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="date"
+                          name="data_nascimento"
+                          id="data_nascimento"
+                          defaultValue={registro.data_nascimento}
+                          onChange={handleOnChange}
+                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:4"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label htmlFor="celular" className="block text-sm font-semibold leading-4 text-gray-900">
+                        Celular
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="tel"
+                          name="celular"
+                          id="celular"
+                          defaultValue={registro.celular}
+                          autoComplete='billing mobile tel'
+                          onChange={handleOnChange}
+                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label htmlFor="email" className="block text-sm font-semibold leading-4 text-gray-900">
+                        E-Mail
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="email"
+                          name="email"
+                          id="email"
+                          defaultValue={registro.email}
+                          onChange={handleOnChange}
+                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label htmlFor="cidade" className="block text-sm font-semibold leading-4 text-gray-900">
+                        Cidade
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="text"
+                          name="cidade"
+                          id="cidade"
+                          defaultValue={registro.cidade}
+                          autoComplete="home city"
+                          onChange={handleOnChange}
+                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => salvar()}
+                  >
+                    Salvar
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,113 +1,131 @@
-import Image from "next/image";
-
-export default function Home() {
+"use client"
+import { useState } from 'react'
+import { redirect } from 'next/navigation'
+import { Switch } from '@headlessui/react'
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+async function getLogin(matricula, cpf) {
+  const body = new URLSearchParams();
+  body.append('matricula', matricula);
+  body.append('cpf', cpf);
+  const res = await fetch(`https://facial.parquedasaguas.com.br/login`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body,
+  });
+  const json = await res.json();
+  localStorage.setItem('token', json['token']);
+  return json['titulo'];
+}
+export default function Login() {
+  const [agreed, setAgreed] = useState(false)
+  const [matricula, setMatricula] = useState('');
+  const [cpf, setCPF] = useState('');
+  
+  const login = () => {
+    if (agreed) {
+      const titulo = getLogin(matricula, cpf);
+      if (titulo)
+        redirect('/cadastro');
+    } else {
+      alert("Você deve concordar com a política de privacidade antes de entrar")
+    }
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="isolate bg-white px-6 py-12 sm:py-32 lg:px-8">
+      <div
+        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="mx-auto max-w-2xl text-center">
+        <img src="https://compre.parquedasaguas.com.br/img/cpa.png" className="w-24 mx-auto" />
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Reconhecimento Facial Sócios</h2>
+        <p className="mt-2 text-lg leading-8 text-gray-600">
+          Efetue o cadastramento e atualização de dados de reconhecimento facial para sócios e dependentes.
+        </p>
       </div>
-    </main>
-  );
+      <form action={login} className="mx-auto mt-16 max-w-xl sm:mt-20">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div>
+            <label htmlFor="matricula" className="block text-sm font-semibold leading-6 text-gray-900">
+              Matrícula
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="matricula"
+                id="matricula"
+                onChange={evt => setMatricula(evt.target.value)}
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="cpf" className="block text-sm font-semibold leading-6 text-gray-900">
+              CPF
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="cpf"
+                id="cpf"
+                onChange={evt => setCPF(evt.target.value)}
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
+            <div className="flex h-6 items-center">
+              <Switch
+                checked={agreed}
+                onChange={setAgreed}
+                className={classNames(
+                  agreed ? 'bg-indigo-600' : 'bg-gray-200',
+                  'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                )}
+              >
+                <span className="sr-only">Estou de acordo com a polícica de privacidade</span>
+                <span
+                  aria-hidden="true"
+                  className={classNames(
+                    agreed ? 'translate-x-3.5' : 'translate-x-0',
+                    'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
+                  )}
+                />
+              </Switch>
+            </div>
+            <Switch.Label className="text-sm leading-6 text-gray-600">
+              Eu concordo com a {' '}
+              <a href="#" className="font-semibold text-indigo-600">
+                Politica de Privacidade (LGPD)
+              </a>
+              .
+            </Switch.Label>
+          </Switch.Group>
+        </div>
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Entrar
+          </button>
+          <div className='pt-8'>
+            <small>Dúvidas Revolution Serviços <a href="//wa.me/5551999926208">(51) 99992-6208</a></small>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
