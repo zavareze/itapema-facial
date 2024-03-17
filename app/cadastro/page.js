07143de66ba4d7972620ce333a748c0d0d13848a
zavjs <henri.zavsss@gmail.com>
2024-03-17 15:25:52 -0300

    usar hook para accessar local storage apenas no cliente

--- app/cadastro/page.js
+++ app/cadastro/page.js
@@ -1,62 +1,90 @@
-"use client"
-import { useState } from 'react'
-import InputMask from 'react-input-mask';
-async function cadastro(req) {
+"use client";
+import { useEffect, useState } from "react";
+import InputMask from "react-input-mask";
+
+function useGetLocalStorageToken() {
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    setToken(localStorage.getItem("token"));
+  }, [token]);
+
+  return [token, setToken];
+}
+
+async function cadastro(req, token, onSuccess) {
   const body = JSON.stringify(req);
-  const res = await fetch(`https://facial.parquedasaguas.com.br/cadastro/titulo`, {
-    // mode: 'no-cors',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': localStorage.getItem('token')
-    },
-    body,
-  });
+  const res = await fetch(
+    `https://facial.parquedasaguas.com.br/cadastro/titulo`,
+    {
+      // mode: 'no-cors',
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      body,
+    }
+  );
   console.log(res);
   const json = await res.json();
   console.log(json);
-  localStorage.setItem('token', json['token']);
-  Router.push('/cadastro');
-  return json
+  onSuccess(json["token"]);
+  Router.push("/cadastro");
+  return json;
 }
+
 export default function Cadastro() {
   const [showModal, setShowModal] = useState(false);
+  const [localStorageToken, setLocalStorageToken] = useGetLocalStorageToken();
   const [registro, setRegistro] = useState({
-    matricula: '',
-    nome: '',
-    cpf: '', 
-    data_nascimento: '',
-    celular: '',
-    email: '',
-    cidade: '',
-    facial: '',
-    updated: ''
+    matricula: "",
+    nome: "",
+    cpf: "",
+    data_nascimento: "",
+    celular: "",
+    email: "",
+    cidade: "",
+    facial: "",
+    updated: "",
   });
   const [updated, setUpdated] = useState({});
-  const handleOnChange = event => {
+  const handleOnChange = (event) => {
     const { name, value } = event.target;
     setUpdated({ ...updated, [name]: value });
   };
   const parseJWT = (token) => {
-      var base64Url = token.split('.')[1];
-      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-      var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      }).join(''));
-      return JSON.parse(jsonPayload);
-  }
-  const jwt = parseJWT(localStorage.getItem('token'));
-  const [titulos, setTitulos] = useState(jwt['titulo']);
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+      window
+        .atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+    return JSON.parse(jsonPayload);
+  };
+  const jwt = localStorageToken ? parseJWT(localStorageToken) : [];
+  const [titulos, setTitulos] = useState(jwt["titulo"]);
+
   const mostraModal = (person) => {
     setRegistro(person);
     setUpdated(person);
     setShowModal(true);
-  }
+  };
+
+  const onSuccess = (token) => {
+    setLocalStorageToken(token);
+  };
+
   const salvar = () => {
     console.log(updated);
-    cadastro(updated);
+    cadastro(updated, localStorageToken, onSuccess);
     setShowModal(false);
-  }
+  };
   return (
     <>
       <div>
@@ -71,20 +99,48 @@ export default function Cadastro() {
           dia com as taxas para poder acessar o clube</p>
         </div>
         <h1 className="text-lg font-bold">Relação de Pessoas no Título</h1>
-        <ul role="list" className="divide-y sm:grid sm:grid-cols-3 sm:gap-4 cursor-pointer">
-          {titulos.map((person) => (
-            <li key={person.email} className={'flex justify-between gap-x-6 py-2 '+ (person.facial=="1"?'bg-green-200':'bg-red-200')}
-              onClick={() => {mostraModal(person)}} key={person.matricula}>
+        <ul
+          role="list"
+          className="divide-y sm:grid sm:grid-cols-3 sm:gap-4 cursor-pointer"
+        >
+          {titulos?.map((person) => (
+            <li
+              key={person.email}
+              className={
+                "flex justify-between gap-x-6 py-2 " +
+                (person.facial == "1" ? "bg-green-200" : "bg-red-200")
+              }
+              onClick={() => {
+                mostraModal(person);
+              }}
+            >
               <div className="flex min-w-0 gap-x-4 px-2">
                 <div className="w-24 flex-none">
-                  <img className="w-40 h-32 rounded-lg" src={'https://sistema.parquedasaguas.com.br/fotos.php?matricula='+person.matricula} alt="" />
+                  <img
+                    className="w-40 h-32 rounded-lg"
+                    src={
+                      "https://sistema.parquedasaguas.com.br/fotos.php?matricula=" +
+                      person.matricula
+                    }
+                    alt=""
+                  />
                 </div>
                 <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">{person.nome}</p>
-                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.cpf}</p>
-                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.celular}</p>
-                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
-                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">Atualizado: {person.updated}</p>
+                  <p className="text-sm font-semibold leading-6 text-gray-900">
+                    {person.nome}
+                  </p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                    {person.cpf}
+                  </p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                    {person.celular}
+                  </p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                    {person.email}
+                  </p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                    Atualizado: {person.updated}
+                  </p>
                 </div>
               </div>
             </li>
@@ -128,7 +184,6 @@ export default function Cadastro() {
                           id="cpf"
                           defaultValue={registro.cpf}
                           onChange={handleOnChange}
-                          onChange={(event) => console.log(event.target.value)}
                           className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
                       </div>
