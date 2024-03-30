0c495cb59e24c500a5bdca0a5aea6527c7f32c4c
Zavareze <github@zavareze.com.br>
2024-03-30 16:16:41 -0300

    change button labels

--- app/cadastro/page.js
+++ app/cadastro/page.js
@@ -302,14 +302,14 @@ export default function Cadastro() {
                     type="button"
                   >
                     <input type="file" name="image" id="upload" accept="image/*" capture="environment" onChange={handleUploadFile}></input>
-                    Procurar na Galeria
+                    Foto Câmera
                   </button>
                   <button
                     className="btn-responsive bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                   >
                     <input type="file" name="image2" id="upload2" accept="image/*" onChange={handleUploadFile}></input>
-                    Foto Camera
+                    Procurar na Galeria
                   </button>
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
