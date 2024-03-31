731b5eb11a00218575ea2e1d8e7aaae2ffa05c29
Zavareze <github@zavareze.com.br>
2024-03-31 17:44:21 -0300

    update photo-button without data

--- app/cadastro/page.js
+++ app/cadastro/page.js
@@ -309,20 +309,23 @@ export default function Cadastro() {
                 </div>
                 {/*footer*/}
                 <div className="flex items-center flex-wrap justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                  <button
-                    className="btn-responsive bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <input type="file" name="image" id="upload" accept="image/*" capture="environment" onChange={handleUploadFile}></input>
-                    Foto Câmera
-                  </button>
-                  <button
-                    className="btn-responsive bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <input type="file" name="image2" id="upload2" accept="image/*" onChange={handleUploadFile}></input>
-                    Procurar na Galeria
-                  </button>
+                  {registro.cpf != '' ? (
+                  <>
+                    <button
+                      className="btn-responsive bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                    >
+                      <input type="file" name="image" id="upload" accept="image/*" capture="user" onChange={handleUploadFile}></input>
+                      Foto Câmera
+                    </button>
+                    <button
+                      className="btn-responsive bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                    >
+                      <input type="file" name="image2" id="upload2" accept="image/*" onChange={handleUploadFile}></input>
+                      Procurar na Galeria
+                    </button>
+                  </>) : (<div className="mb-4">Após salvar todos os dados você poderá enviar a Foto para efetuar o reconhecimento facial</div>)}
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
